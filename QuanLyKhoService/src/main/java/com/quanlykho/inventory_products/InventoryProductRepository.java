98ee8f43c92766d3570fe4a3686aea3e321c0ade
Kaneki377 <69791816+Kaneki377@users.noreply.github.com>
2024-10-16 21:06:24 +0700

    Sửa lỗi entity

--- QuanLyKhoService/src/main/java/com/quanlykho/inventory_products/InventoryProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quanlykho.inventory_products;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.quanlykho.common.Product;
-
-public interface InventoryProductRepository extends JpaRepository<Product, Integer> {
-
-}
